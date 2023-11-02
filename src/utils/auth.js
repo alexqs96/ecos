fd5987c6e66bc4daa0be2a8fd9c50a1adff6abe5
alexqs96 <alexander.mamani.dev@gmail.com>
2023-11-02 13:04:41 -0300

    removed unused dependencies

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,30 +1,4 @@
 import bcrypt from 'bcryptjs'
-import { SignJWT } from 'jose';
-
-export const createToken = async (payload) => {
-	return new SignJWT(payload)
-		.setProtectedHeader({ alg: "HS256" })
-		.setSubject(payload.sub)
-		.setExpirationTime("1d")
-		.sign(new TextEncoder().encode(process.env.JWT_SECRET))
-};
-
-export const verifyJWT = async (token) => {
-  try {
-    return (
-      await jwtVerify(
-        token,
-        new TextEncoder().encode(process.env.JWT_SECRET)
-      )
-    ).payload
-  } catch (error) {
-    if (error) {
-      throw new Error('El token ha expirado');
-    } else {
-      throw new Error('Error al verificar el token');
-    }
-  }
-};
 
 export const hashPassword = async (password) => {
   const saltRounds = 10;
