348c62cb773d1fb93bdfc2ac231a5fa1d04f625d
alexqs96 <alexander.mamani.dev@gmail.com>
2023-11-10 16:59:56 -0300

    added vegetables route

---
+++ src/app/api/info/route.js
@@ -0,0 +1,21 @@
+import { connectMongo } from "@/lib/connectMongo";
+import Database from "@/lib/models/Database";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await connectMongo();
+
+    const data = await Database.find().lean();
+
+    return NextResponse.json(data, {
+      status: 200,
+      statusText: "Info",
+    });
+  } catch (error) {
+    return NextResponse.json(data, {
+      status: 500,
+      statusText: "Error Info",
+    });
+  }
+}
