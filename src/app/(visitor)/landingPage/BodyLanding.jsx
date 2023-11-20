acc9106597d093d360da65be4073cfa544c7c53d
Rodrigo <contactorodrigocabrera@gmail.com>
2023-11-20 16:21:22 -0300

    landing desktop finalizado

--- src/app/(visitor)/landingPage/BodyLanding.jsx
+++ src/app/(visitor)/landingPage/BodyLanding.jsx
@@ -1,57 +1,101 @@
 import React from 'react';
-import PlantasLanding from '../../../images/plantas.png'
-import plantas1 from '../../../images/Group1plant.svg'
+import Image from 'next/image';
 
 export default function BodyLanding() {
     return (
-        <div className="bg-white h-90vh w-screen flex flex-col ">
+        <div className="bg-white h-90vh w-screen flex flex-col items-center gap-y-14 pb-10">
 
-            <div className='h-full h-1/2 flex space-around'>
-                <div className=" w-1/2">
-                    <p className='text-black m-8'>Entra a la comunidad, intercambia tus cultivos e infórmate para ser el mejor cultivador. </p>
+            <div className='w-10/12 flex space-around py-10'>
+                <div className=" w-7/12 content-items-center">
+                    <p className='text-black font-semibold text-5xl m-8'>Entra a la comunidad, intercambia tus cultivos e infórmate para ser el mejor cultivador. </p>
                 </div>
 
-                <div className="w-1/2">
-                    <img src={PlantasLanding} alt="imagenPlantas" />
+                <div className="w-4/12">
+                    <Image
+                        src={"/img/Landing1.png"}
+                        width={400}
+                        height={400}
+                        alt="Picture of the plant"
+                        unoptimized
+                    />
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <p className='text-black m-8'>¡Conecta con la comunidad!</p>
-                    <img src={plantas1} alt="plantas1" />
+            <div className='w-10/12 flex justify-evenly'>
+                <div className='border-2 drop-shadow-lg rounded-lg w-3/12 flex flex-col place-items-center text-center pb-8'>
+                    <p className='text-green-800 p-4 text-xl font-semibold'>¡Conecta en comunidad!</p>
+                    <Image 
+                        src="/img/Landing2.png"
+                        width={200}
+                        height={200}
+                        alt="Picture of the plant"
+                        unoptimized
+                    />
                 </div>
-                <div>
-                    <p className='text-black m-8'>¡Crea tu huerta!</p>
+                <div  className='border-2 drop-shadow-lg rounded-lg w-3/12 flex flex-col place-items-center text-center'>
+                    <p className='text-green-800 p-4 text-xl font-semibold'>¡Crea tu huerta!</p>
+                    <Image
+                        src="/img/Landing3.png"
+                        width={200}
+                        height={200}
+                        alt="Picture of the plant"
+                        unoptimized
+                    />
                 </div>
-                <div>
-                    <p className='text-black m-8'>¡Aprende sobre plantas!</p>
+                <div  className='border-2 drop-shadow-xl rounded-lg w-3/12 flex flex-col place-items-center text-center'>
+                    <p className='text-green-800 p-4 text-xl font-semibold'>¡Aprende sobre plantas!</p>
+                    <Image
+                        src="/img/Landing4.png"
+                        width={200}
+                        height={200}
+                        alt="Picture of the plant"
+                        unoptimized
+                    />
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <h2 className='text-black m-8'>Cultiva e intercambia</h2>
-                    <p className='text-black m-8'>¡Convierte tu pasión por la jardinería en conexiones reales! Comparte, intercambia y crece junto a otros amantes de las plantas en nuestra comunidad. Comienza hoy a intercambiar tus plantas, frutas y verduras favoritas.</p>
-                    <button className="flex flex-col justify-center items-center gap-8 flex-shrink-0 rounded-full bg-green-500">
-                        <p>¡Comienza a intercambiar!</p>
-                    </button>
+            <div className='w-10/12 flex space-around'>
+                <div className='w-6/12'>
+                    <h2 className='text-black m-8 text-4xl font-semibold'>Cultiva e intercambia</h2>
+                    <p className='text-black m-8 text-2xl text-gray-500'>¡Convierte tu pasión por la jardinería en conexiones reales! Comparte, intercambia y crece junto a otros amantes de las plantas en nuestra comunidad. Comienza hoy a intercambiar tus plantas, frutas y verduras favoritas.</p>
+                    <div className='flex flex-col justify-center items-center '>
+                        <button className="gap-8 flex-shrink-0 rounded-full bg-green-500 text-xl p-4">
+                            <p>¡Comienza a intercambiar!</p>
+                        </button>
+                    </div>
                 </div>
-                <div></div>
+                <div className='w-6/12 p-4 flex place-items-center'>
+                    <Image
+                        src="/img/Landing5.png"
+                        width={500}
+                        height={500}
+                        alt="Picture of the plant"
+                        unoptimized
+                    />
+                </div>
             </div>
 
-            <div>
-                <div>
-                    {/* aqui va una imagen */}
+            <div className='w-10/12 flex space-around'>
+                <div className='w-6/12 p-4 flex place-items-center'>
+                    <Image
+                        src="/img/Landing6.png"
+                        width={400}
+                        height={400}
+                        alt="Picture of the plant"
+                        unoptimized
+                    />
                 </div>
-                <div>
-                <h2 className='text-black m-8'>Infórmate y diviértete cultivando</h2>
-                    <p className='text-black m-8'>Descubre un mundo de diversión y aprendizaje en tu jardín virtual. Sumérgete en la magia de la agricultura, comparte tus experiencias y descubre nuevos trucos. Cultivar nunca fue tan emocionante. ¡Únete a la comunidad hoy!</p>
-                    <button className="flex flex-col justify-center items-center gap-8 flex-shrink-0 rounded-full bg-green-500">
-                        <p>¡Comienza a intercambiar!</p>
-                    </button>
+                <div className='w-6/12'>
+                    <h2 className='text-black m-8 text-4xl font-semibold'>Infórmate y diviértete cultivando</h2>
+                    <p className='text-black m-8 text-2xl text-gray-500'>Descubre un mundo de diversión y aprendizaje en tu jardín virtual. Sumérgete en la magia de la agricultura, comparte tus experiencias y descubre nuevos trucos. Cultivar nunca fue tan emocionante. ¡Únete a la comunidad hoy!</p>
+                    <div className='flex flex-col justify-center items-center '>
+                        <button className="gap-8 flex-shrink-0 rounded-full bg-green-500 text-xl p-4">
+                            <p>¡Comienza a intercambiar!</p>
+                        </button>
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 }
--- src/app/(visitor)/landingPage/HeaderLanding.jsx
+++ src/app/(visitor)/landingPage/HeaderLanding.jsx
@@ -9,28 +9,31 @@ export default function HeaderLanding() {
     const { data: session, status } = useSession();
 
     return (
-        <header className="bg-green-500 h-10vh w-screen flex items-center justify-evenly p-2">
-            <div className="text-white text-lg font-bold">
-                <EcosLogo className=""/>
-                Ecos
+        <header className="bg-green-500 h-10vh w-screen flex justify-center py-2">
+            <div className='flex w-10/12 flex justify-between'>
+                <div className="text-white text-3xl font-bold flex items-center gap-x-3">
+                    <EcosLogo className=""/>
+                    Ecos
                 </div>
-            <div className="flex space-x-4">
-                {
-                    status === "loading"?
-                    <AiOutlineLoading size={24} className="animate-spin" />
-                    :
-                    status === "authenticated"?
-                    <Link href="/home" className="border p-2 rounded-md">Ir al inicio @{session?.user?.username}</Link>
-                    :
-                    <>
-                    <button className="flex flex-col justify-center items-center gap-8 flex-shrink-0 rounded-full bg-purple-500">
-                        <Link href="/signup" className="text-white m-4">Registrarse</Link>
-                    </button>
-                    <button className="flex items-center justify-center flex-shrink-0 flex-col gap-8 rounded-full border border-purple-600 bg-white">
-                        <Link href="/signin" className="text-black m-4">Iniciar Sesión</Link>
-                    </button>
-                    </>
-                }
+                
+                <div className="flex space-x-4">
+                    {
+                        status === "loading"?
+                        <AiOutlineLoading size={24} className="animate-spin" />
+                        :
+                        status === "authenticated"?
+                        <Link href="/home" className="border p-2 rounded-md">Ir al inicio @{session?.user?.username}</Link>
+                        :
+                        <>
+                        <button className="flex flex-col justify-center items-center gap-8 flex-shrink-0 rounded-full bg-fuchsia-400 px-10 py-3">
+                            <Link href="/signup" className="text-white font-bold">Registrarse</Link>
+                        </button>
+                        <button className="flex items-center justify-center flex-shrink-0 flex-col gap-8 rounded-full border border-fuchsia-800 bg-white px-10 py-3">
+                            <Link href="/signin" className="text-fuchsia-400 font-bold">Iniciar Sesión</Link>
+                        </button>
+                        </>
+                    }
+                </div>
             </div>
         </header>
     );
