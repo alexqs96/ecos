25398f8572ceaaf4074d8ccfaba08e572ba4bf77
alexqs96 <alexander.mamani.dev@gmail.com>
2023-11-22 21:58:56 -0300

    fix sidebar

--- src/app/(user)/home/SideNavbar.jsx
+++ src/app/(user)/home/SideNavbar.jsx
@@ -1,6 +1,7 @@
 'use client'
 import { GardenIcon, HeartIcon, LogoutIcon, MessageIcon, PlantIcon, ProfileIcon, TradeIcon } from "@/components/Icons";
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -43,25 +44,27 @@ const SideNavbar = () => {
   const { data: session, status } = useSession();
 
   return (
-    <nav className="max-sm:hidden md:min-w-[300px] lg:max-w-[353px] py-5 bg-[#27b53C]">
-      <div className="flex flex-col gap-5 fixed top-0">
-        <Link href={`/profile`} className="flex gap-3 items-center pl-[14%]">
-          <img
-            // src={`${session?.user?.image} || "/avatar.svg"`}
-            src="./avatar.svg"
+    <nav className="max-sm:hidden w-full lg:max-w-xs">
+      <div className="flex flex-col gap-5 bg-[#27b53C] fixed h-[100dvh] py-5 w-full max-w-xs">
+        <Link href={`/${session?.user?.username}`} className="flex gap-3 items-center pl-[14%]">
+          <Image
+            src={session?.user?.image || "/img/profile_default.webp"}
             alt="profile"
             className="h-14 w-14 rounded-full"
+            width={100}
+            height={100}
+            unoptimized
           />
           <div className="flex flex-col">
             <p className="body-bold font-bold text-white">
               {session?.user?.name}
             </p>
             <p className="text-sm font-normal text-white">
-              @{session?.user?.name}
+              @{session?.user?.username}
             </p>
           </div>
         </Link>
-        <hr className="w-[255px] ml-[14.8%] " />
+        <hr className="border-white/30" />
         <ul className="flex flex-col gap-6 font-semibold text-white">
           {SIDEBAR_NAV_LINKS.map((link) => {
             const isActive = link.href === pathname;
@@ -73,8 +76,7 @@ const SideNavbar = () => {
                   isActive && "bg-white text-[#27b53C]"
                 } pl-[12.5%]`}
               >
-                <Link href={link.href} className="flex gap-4 items-center p-4">
-                  
+                <Link href={link.href === "/profile"? "/"+session?.user?.username : link.href} className="flex gap-4 items-center p-4">
                   {link.icon}
                   {link.label}
                 </Link>
@@ -82,14 +84,14 @@ const SideNavbar = () => {
             );
           })}
         </ul>
+        <button
+          onClick={() => signOut()}
+          className="flex gap-3 items-center pl-[17%] py-12"
+        >
+          <LogoutIcon size={24} className="fill-white"/>
+          <p className="font-regular text-white">Salir</p>
+        </button>
       </div>
-      <button
-        onClick={() => signOut()}
-        className="flex gap-3 items-center pl-[17%] py-12"
-      >
-        <LogoutIcon size={24} className="fill-white"/>
-        <p className="font-regular text-white">Salir</p>
-      </button>
     </nav>
   );
 };
