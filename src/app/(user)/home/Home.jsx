ff81206f566efd63d8fb39d078cff2ac64747646
alexqs96 <alexander.mamani.dev@gmail.com>
2023-11-27 19:40:45 -0300

    fix bugs api and ui

--- src/app/(user)/home/Home.jsx
+++ src/app/(user)/home/Home.jsx
@@ -15,16 +15,18 @@ export default function HomePage() {
       <Toaster />
       <section className='grid gap-5 w-full mb-10 p-5'>
         {
-          session?.user?
-          <h1 className="text-3xl font-semibold flex items-center gap-2 text-[#27b53C]">
-            <PiHouseFill size={"1.2em"} className={"fill-[#27b53C]"} />
-            Comunidad
-          </h1>
-          :
-          null
+          session?.user ?
+            <>
+              <h1 className="text-3xl font-semibold flex items-center gap-2 text-[#27b53C]">
+                <PiHouseFill size={"1.2em"} className={"fill-[#27b53C]"} />
+                Comunidad
+              </h1>
+              <PostForm />
+              <Posts endpoint={"/api/posts"} />
+            </>
+            :
+            null
         }
-        <PostForm />
-        <Posts endpoint={"/api/posts"}/>
       </section>
       <ContactsSidebar />
     </>
