dd1db476a239fc68aef03673a32db10bf7379ad6
AgustinC91 <nahuel.cordoba91@gmail.com>
2023-11-27 23:15:34 -0300

    Quick info finish

--- src/app/(user)/plants/page.jsx
+++ src/app/(user)/plants/page.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import PlantList from './PlantList'
-import ContactsSidebar from '@/components/ContactsSidebar'
+import Favourites from '@/components/Favourites'
 
 const PlantsPage = ({}) => {
   return (
     <>
     <PlantList />
-    <ContactsSidebar />
+    <Favourites />
     </>
   )
 }
---
+++ src/components/Favourites.jsx
@@ -0,0 +1,13 @@
+
+export default function Favourites() {
+
+  return (
+    <aside className="max-xl:hidden max-w-xs w-full">
+      <div className="fixed w-full max-w-xs h-[100dvh] p-5 top-0">
+        <>
+          <h2 className="text-2xl font-semibold text-[#27b53C]">Mis Favoritos</h2>
+        </>
+      </div>
+    </aside>
+  )
+}
