fb56b34cbbb6047ce02a0592dada4912b4c78286
AgustinC91 <nahuel.cordoba91@gmail.com>
2023-11-28 19:51:32 -0300

    responsive plants finalizado

--- src/app/(user)/plants/[slug]/MyDisclosure.jsx
+++ src/app/(user)/plants/[slug]/MyDisclosure.jsx
@@ -8,7 +8,7 @@ export const MyDisclosure = ({title, text}) => {
         <Disclosure >
           {({ open }) => (
             <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-[#BCF0C4] px-4 py-2 text-left text-sm font-medium text-[#0C3712] hover:bg-[#BCF0C4] focus:outline-none focus-visible:ring focus-visible:ring-[#2DD246]">
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-[#BCF0C4] px-4 py-2 text-left text-xl font-medium text-[#0C3712] hover:bg-[#BCF0C4] focus:outline-none focus-visible:ring focus-visible:ring-[#2DD246]">
                 <span>{title}</span>
                 <ChevronIcon
                   className={"transition-transform duration-300 "+`${
@@ -24,7 +24,7 @@ export const MyDisclosure = ({title, text}) => {
                 leaveFrom="transform scale-100 opacity-100"
                 leaveTo="transform scale-95 opacity-0"
               >
-                <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500 bg-[#F2FDF3]">
+                <Disclosure.Panel className="px-4 pb-2 pt-4 text-md text-gray-500 bg-[#F2FDF3]">
                 {text}
                 </Disclosure.Panel>
               </Transition>
