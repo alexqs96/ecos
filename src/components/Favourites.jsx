81bc044a81ae9d7d28c83ceaaf9d6d443cd0c4ee
AgustinC91 <nahuel.cordoba91@gmail.com>
2023-11-28 03:30:57 -0300

    fav section

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,13 +1,12 @@
-
 export default function Favourites() {
-
   return (
-    <aside className="max-xl:hidden max-w-xs w-full">
-      <div className="fixed w-full max-w-xs h-[100dvh] p-5 top-0">
-        <>
-          <h2 className="text-2xl font-semibold text-[#27b53C]">Mis Favoritos</h2>
-        </>
+    <aside className="max-xl:hidden max-w-xs w-full border-l-2 flex flex-col ">
+      <div className="w-full max-w-xs h-[100dvh] p-5 top-0 ">
+        <h2 className="text-2xl font-semibold text-[#27b53C] text-center py-4">Mis Favoritos</h2>
+        <div className="bg-gray-200 h-[300px] rounded-xl py-6">
+
+        </div>
       </div>
     </aside>
-  )
+  );
 }
